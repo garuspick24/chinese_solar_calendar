9d461e68b5f4c8d877b6205c3563369fa3ec361c
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-03-24 23:28:31 +0200

    визначили дати зміни місяців і років

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import sqlite3
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.ephem.ephem import nextSolarReturn
 from flatlib.geopos import GeoPos
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS calendar (
+        id INTEGER PRIMARY KEY,
+        date TEXT,
+        year_stem TEXT,
+        year_branch TEXT
+    )
+''')
+conn.commit()
 
 # Build a chart for a date and location
 date = Datetime('2024/03/24', '17:00', '+02:00')
@@ -21,3 +35,16 @@ xiu = nextSolarReturn(date, 195)
 hai = nextSolarReturn(date, 225)
 zi = nextSolarReturn(date, 255)
 chou = nextSolarReturn(date, 285)
+
+year_stem = "Jia"
+year_branch = "Zi"
+
+data = (str(date), year_stem, year_branch)
+
+cursor.execute(
+    'INSERT INTO calendar (date, year_stem, year_branch) VALUES (?, ?, ?)',
+    data)
+conn.commit()
+
+# Закриття підключення
+conn.close()
